c5c01f304560a62adfafdc8776fe96d22c606070
Konstantin Potapov <pkbo@users.sourceforge.net>
2011-08-27 10:00:38 +0700

    workaround changes for build using PB SDK by sergeyvl

--- tools/toolchain-arm-pocketbook.cmake
+++ tools/toolchain-arm-pocketbook.cmake
@@ -6,15 +6,18 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   /usr/local/pocketbook/bin/arm-linux-gcc)
-SET(CMAKE_CXX_COMPILER /usr/local/pocketbook/bin/arm-linux-g++)
-SET(CMAKE_STRIP /usr/local/pocketbook/bin/arm-linux-strip)
+SET(CMAKE_C_COMPILER   arm-linux-gcc)
+SET(CMAKE_CXX_COMPILER arm-linux-g++)
+SET(CMAKE_STRIP arm-linux-strip)
 
 # where is the target environment 
-SET(CMAKE_FIND_ROOT_PATH  /usr/local/pocketbook/arm-linux)
+SET(CMAKE_FIND_ROOT_PATH  /usr/arm-linux)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # for libraries and headers in the target directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+include_directories(/usr/arm-linux/include/c++/3.4.1)
+include_directories(/usr/arm-linux/include/c++/3.4.1/arm-linux)
