a15155ab59128de73979d652ce3bccd5671802b9
Konstantin Potapov <pkbo@users.sourceforge.net>
2011-09-03 19:06:08 +0700

    change version

--- cr3gui/src/cr3pocketbook.h
+++ cr3gui/src/cr3pocketbook.h
@@ -53,8 +53,8 @@ enum CRPbCommands {
 
 #define PB_CR3_CACHE_SIZE (0x100000 * 64)
 
-#define CR_PB_VERSION "0.0.6-1"
-#define CR_PB_BUILD_DATE "2011-08-14"
+#define CR_PB_VERSION "0.0.6-2"
+#define CR_PB_BUILD_DATE "2011-09-03"
 
 #define PB_ROTATE_MODE_360 0
 #define PB_ROTATE_MODE_180 1
