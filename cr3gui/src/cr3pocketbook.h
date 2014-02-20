bb1bdb363cfc438d3f6a30a869534ce5cd96af18
Konstantin Potapov <pkbo@users.sourceforge.net>
2014-02-20 21:33:41 +0700

    Increment version

--- cr3gui/src/cr3pocketbook.h
+++ cr3gui/src/cr3pocketbook.h
@@ -54,8 +54,8 @@ enum CRPbCommands {
 
 #define PB_CR3_CACHE_SIZE (0x100000 * 64)
 
-#define CR_PB_VERSION "0.0.6-15"
-#define CR_PB_BUILD_DATE "2014-02-17"
+#define CR_PB_VERSION "0.0.6-16"
+#define CR_PB_BUILD_DATE "2014-02-18"
 
 #define PB_ROTATE_MODE_360 0
 #define PB_ROTATE_MODE_180 1
