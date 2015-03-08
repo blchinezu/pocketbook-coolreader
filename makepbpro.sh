87349fbf88cfd59c187813f1562556726190feed
Bruce Lee <eu.brucelee@yahoo.com>
2015-03-08 13:45:57 +0200

    Added build scripts

---
+++ makepbpro.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+mkdir -p pbPro
+cd pbPro
+cmake \
+    -D INKVIEW_LIB=/home/brucelee/PBDEV/FRSCSDK/arm-none-linux-gnueabi/sysroot/usr/lib/libinkview.so \
+    -D CMAKE_TOOLCHAIN_FILE=../tools/toolchain-arm-gnu-eabi-pocketbook.cmake \
+    -D TARGET_TYPE=ARM \
+    -D DEVICE_NAME=pb360 \
+    -D MAX_IMAGE_SCALE_MUL=2 \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D ENABLE_CHM=1 \
+    -D ENABLE_ANTIWORD=1 \
+    -D GUI=CRGUI_PB \
+    -D POCKETBOOK_PRO=1 \
+    ..
+make
+
+if [ -f pbPro/cr3gui/cr3-pb.app ]; then
+    ../../FRSCSDK/bin/arm-none-linux-gnueabi-strip pbPro/cr3gui/cr3-pb.app
+fi
