6e94ecc5eb6bdfc2f9c1c740d13dc7716e4c6dc2
Bruce Lee <eu.gabii@yahoo.com>
2015-03-25 14:02:25 +0200

    updated ota code

--- cr3gui/src/ota_update.h
+++ cr3gui/src/ota_update.h
@@ -2,14 +2,30 @@
 #define OTA_UPDATE_H
 
 
-#define PB_OTA_URL_BASE      "http://sourceforge.net/u/e379d14417/crengine/ci/pocketbook-port/tree/builds/"
-#define PB_OTA_URL_MASK      PB_OTA_URL_BASE "[DEVICE]/"CR_PB_SDK"/latest.zip?format=raw"
-#define PB_OTA_URL_MASK_TEST PB_OTA_URL_BASE "[DEVICE]/"CR_PB_SDK"/exists?format=raw"
-#define PB_OTA_VERSION       PB_OTA_URL_BASE "current.version?format=raw"
-#define PB_OTA_LINK_MASK     PB_OTA_URL_BASE "links/[DEVICE]?format=raw"
-#define PB_OTA_VERSION_MAX_LENGTH 15
-#define PB_OTA_LINK_MAX_LENGTH    50
-#define PB_OTA_EXISTS_STR         "yes"
+#ifndef TEMPDIR
+    #ifndef DIRPREFIX
+    #define DIRPREFIX ""
+    #endif
+
+    #define TEMPDIR DIRPREFIX "/tmp"
+#endif
+
+
+#define OTA_DOWNLOAD_DIR TEMPDIR
+#define OTA_PACKAGE_NAME "latest.zip"
+
+
+#define OTA_URL_BASE      "http://sourceforge.net/u/e379d14417/crengine/ci/pocketbook-port/tree/builds/"
+#define OTA_URL_POST      "?format=raw"
+#define OTA_URL_MASK      OTA_URL_BASE "[DEVICE]/" CR_PB_SDK "/" OTA_PACKAGE_NAME OTA_URL_POST
+#define OTA_URL_MASK_TEST OTA_URL_BASE "[DEVICE]/" CR_PB_SDK "/exists" OTA_URL_POST
+#define OTA_VERSION       OTA_URL_BASE "current.version" OTA_URL_POST
+#define OTA_LINK_MASK     OTA_URL_BASE "links/[DEVICE]" OTA_URL_POST
+
+
+#define OTA_VERSION_MAX_LENGTH 15
+#define OTA_LINK_MAX_LENGTH    50
+#define OTA_EXISTS_STR         "yes"
 
 
 bool OTA_update();
