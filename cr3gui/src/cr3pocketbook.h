3db26e67d51de14fc97c01ef43ca57294b7f4304
Bruce Lee <eu.gabii@yahoo.com>
2015-06-12 08:10:37 +0300

    Updated freetype to v2.6

--- cr3gui/src/cr3pocketbook.h
+++ cr3gui/src/cr3pocketbook.h
@@ -76,7 +76,7 @@ enum CRPbCommands {
 #define PB_CR3_CACHE_SIZE (0x100000 * 64)
 
 #define CR_PB_VERSION "0.0.6-17-23"
-#define CR_PB_BUILD_DATE "2015-05-14"
+#define CR_PB_BUILD_DATE "2015-06-12"
 
 #define PB_ROTATE_MODE_360 0
 #define PB_ROTATE_MODE_180 1
