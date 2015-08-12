362fb52d144f2c211044fcbf49db40cff5333029
Bruce Lee <eu.gabii@yahoo.com>
2015-08-12 17:50:58 +0300

    Renamed string so that it uses existing translation

--- cr3gui/src/viewdlg.cpp
+++ cr3gui/src/viewdlg.cpp
@@ -588,7 +588,7 @@ static const char * getCommandName( int command )
     case PB_CMD_FULL_UPDATE: return _("Full screen update");
     case PB_CMD_STATUS_LINE: return _("Toggle status bar");
     case PB_CMD_SET_BOOKMARK: return _("Set bookmark");
-    case PB_CMD_UNSET_BOOKMARK: return _("Remove bookmark");
+    case PB_CMD_UNSET_BOOKMARK: return _("Delete bookmark");
     #ifdef POCKETBOOK_PRO
     case PB_CMD_TASK_MANAGER: return _("Tasks list");
     #ifndef POCKETBOOK_PRO_PRO2
