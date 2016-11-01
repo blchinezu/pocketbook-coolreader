74c16989819728e4c1f6a7418ab45f32b671f0c6
Bruce Lee <eu.gabii@yahoo.com>
2016-11-01 13:53:09 +0200

    Up version: 0.0.6-17-60.1

--- cr3gui/src/cr3pocketbook.h
+++ cr3gui/src/cr3pocketbook.h
@@ -78,8 +78,8 @@ enum CRPbCommands {
 
 #define PB_CR3_CACHE_SIZE (0x100000 * 64)
 
-#define CR_PB_VERSION "0.0.6-17-60"
-#define CR_PB_BUILD_DATE "2016-10-31"
+#define CR_PB_VERSION "0.0.6-17-60.1"
+#define CR_PB_BUILD_DATE "2016-11-01"
 
 #define PB_ROTATE_MODE_360 0
 #define PB_ROTATE_MODE_180 1
